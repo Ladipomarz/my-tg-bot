eba29c9d99709a0775648fb340946139cbb43dd9
Ladipomarz <ladipomarz@proton.me>
2025-12-18 11:49:07 -0800

    check

--- My_bot/config.py
+++ My_bot/config.py
@@ -5,4 +5,4 @@ DATABASE_URL = os.getenv("DATABASE_URL")  # from Railway
 
 ADMIN_IDS = [123456789]  # keep your real admin IDs
 if not BOT_TOKEN:
-    raise RuntimeError("BOT_TOKEN not set")
+    raise RuntimeError("BOT_TOKEN is missing set")
