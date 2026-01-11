88231db1b21d9936452bf7627564a052274cf646
Ladipomarz <ladipomarz@proton.me>
2026-01-11 12:23:29 -0800

    resr

--- My_bot/handlers/provider_factory.py
+++ My_bot/handlers/provider_factory.py
@@ -2,10 +2,9 @@ from handlers.textverified_provider import TextVerifiedProvider
 from config import OTP_PROVIDER_MODE, API_KEY
 
 def get_otp_provider(api_key=None):
+    print(f"OTP_PROVIDER_MODE: {OTP_PROVIDER_MODE}")  # Debugging line
     if OTP_PROVIDER_MODE == "textverified":
-        print("Using TextVerifiedProvider for live mode.")
+        print("Using TextVerifiedProvider for live mode.")  # Debugging line
         return TextVerifiedProvider(api_key)
     else:
         raise ValueError("Invalid OTP_PROVIDER_MODE setting.")
-
-
