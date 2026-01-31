8188549f756e8d751208e6d0ce7a0d9f91a73877
Ladipomarz <ladipomarz@proton.me>
2026-01-30 17:43:41 -0800

    nmm

--- My_bot/handlers/tools.py
+++ My_bot/handlers/tools.py
@@ -25,7 +25,6 @@ from handlers.otp_handler import send_services_txt
 from handlers.service_list_flow import start_service_list_flow
 from handlers.otp_handler import otp_refund_now_cb
 from handlers.payments import safe_edit_message
-from handlers.rental_handler import fetch_rental_number_for_rental
 
 from utils.validator import (
     is_valid_email,
