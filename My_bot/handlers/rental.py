c80abb81661e353a39bb837ed783cb2c436b4145
Ladipomarz <ladipomarz@proton.me>
2026-03-02 01:01:11 -0800

    jh

--- My_bot/handlers/rental.py
+++ My_bot/handlers/rental.py
@@ -407,12 +407,12 @@ async def manage_rental_menu(update, context):
     
     # 5. Show the beautiful UI
     menu_text = (
-        f"📱 **Number Details**\n\n"
-        f"**ID:** `{rental_id}`\n"
+        f"📱 **Number Details**\n\n",
         f"**Number:** `{phone}`\n"
         f"**Service:** {service.capitalize()}\n"
         f"**Status:** 🟢 Active\n\n"
-        f"**⚠️ Please note this number will expire in - {countdown_str}**\n\n"
+        f"**ID:** `{rental_id}`\n"
+        f"<b>**⚠️ Please note this number will expire in - {countdown_str}**</b>\n\n"
         f"Click the button below to connect to the network and fetch your messages."
     )
     
